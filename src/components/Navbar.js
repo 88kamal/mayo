dac003cafb1401d004c85680d85e0ca7e350bdb4
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-12-03 17:10:57 +0530

    all done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import { BsTelephoneFill, BsWhatsapp } from 'react-icons/bs'
 import { Link } from 'react-router-dom';
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 function Navbar() {
     // const [isExpanded, toggleExpansion] = useState(false);
@@ -34,69 +41,69 @@ function Navbar() {
                                         onClick={() => setNavbar(!navbar)}
                                     >
                                         {navbar ? (
-                                        //     <svg
-                                        //         xmlns="http://www.w3.org/2000/svg"
-                                        //         className="w-6 h-6"
-                                        //         viewBox="0 0 20 20"
-                                        //         fill="currentColor"
-                                        //     >
-                                        //         <path
-                                        //             fillRule="evenodd"
-                                        //             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                        //             clipRule="evenodd"
-                                        //             className=' text-white '
+                                            //     <svg
+                                            //         xmlns="http://www.w3.org/2000/svg"
+                                            //         className="w-6 h-6"
+                                            //         viewBox="0 0 20 20"
+                                            //         fill="currentColor"
+                                            //     >
+                                            //         <path
+                                            //             fillRule="evenodd"
+                                            //             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            //             clipRule="evenodd"
+                                            //             className=' text-white '
 
-                                        //         />
-                                        //     </svg>
-                                        // ) : (
-                                        //     <svg
-                                        //         xmlns="http://www.w3.org/2000/svg"
-                                        //         className="w-9 h-9"
-                                        //         fill="none"
-                                        //         viewBox="0 0 24 24"
-                                        //         stroke="currentColor"
-                                        //         strokeWidth={2}
-                                        //     >
-                                        //         <path
-                                        //             strokeLinecap="round"
-                                        //             strokeLinejoin="round"
-                                        //             d="M4 6h16M4 12h16M4 18h16"
-                                        //             className=' text-white '
+                                            //         />
+                                            //     </svg>
+                                            // ) : (
+                                            //     <svg
+                                            //         xmlns="http://www.w3.org/2000/svg"
+                                            //         className="w-9 h-9"
+                                            //         fill="none"
+                                            //         viewBox="0 0 24 24"
+                                            //         stroke="currentColor"
+                                            //         strokeWidth={2}
+                                            //     >
+                                            //         <path
+                                            //             strokeLinecap="round"
+                                            //             strokeLinejoin="round"
+                                            //             d="M4 6h16M4 12h16M4 18h16"
+                                            //             className=' text-white '
 
-                                        //         />
-                                        //     </svg>
-                                        <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-8 h-8"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                            className=' text-white'
-                                           
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-8 h-8"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                            className=' text-white'
-                                         
+                                            //         />
+                                            //     </svg>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-8 h-8"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                    className=' text-white'
 
-                                        />
-                                    </svg>
+                                                />
+                                            </svg>
+                                        ) : (
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-8 h-8"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                strokeWidth={2}
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M4 6h16M4 12h16M4 18h16"
+                                                    className=' text-white'
+
+
+                                                />
+                                            </svg>
                                         )}
                                     </button>
                                 </div>
@@ -128,6 +135,84 @@ function Navbar() {
                                     </li>
                                     <li className="text-gray-100 hover:text-[#FFA700]">
                                         <Link to={'/contactus'}>Contact us</Link>
+                                        {/* <Menu as="div" className="relative inline-block text-left">
+                                            <div>
+                                                <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                                                    Options
+                                                    <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                                                </Menu.Button>
+                                            </div>
+
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-100"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100 scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95"
+                                            >
+                                                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <div className="py-1">
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                        'block px-4 py-2 text-sm'
+                                                                    )}
+                                                                >
+                                                                    Account settings
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                        'block px-4 py-2 text-sm'
+                                                                    )}
+                                                                >
+                                                                    Support
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                        'block px-4 py-2 text-sm'
+                                                                    )}
+                                                                >
+                                                                    License
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                        <form method="POST" action="#">
+                                                            <Menu.Item>
+                                                                {({ active }) => (
+                                                                    <button
+                                                                        type="submit"
+                                                                        className={classNames(
+                                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                            'block w-full px-4 py-2 text-left text-sm'
+                                                                        )}
+                                                                    >
+                                                                        Sign out
+                                                                    </button>
+                                                                )}
+                                                            </Menu.Item>
+                                                        </form>
+                                                    </div>
+                                                </Menu.Items>
+                                            </Transition>
+                                        </Menu> */}
+
                                     </li>
                                 </ul>
                             </div>
--- src/pages/Admission.js
+++ src/pages/Admission.js
@@ -3,8 +3,36 @@ import Layout from '../components/Layout'
 
 function Admission() {
   return (
-    <Layout>
-        <h1 className='my-20'>Admission</h1>
+    <Layout className="  ">
+
+      <div className='  bg-[#990000] w-full h-[120em]  '>
+        <div className='container mx-auto '>
+          <div className=' flex justify-center'>
+            {/* <img className=" w-[100%] my-[139px]" src="image/Pasted Graphic_1.jpg" alt="" /> */}
+            <img class="md:max-w-[100%] max-w-[90%] h-auto my-[92px] md:my-[139px]" src="image/Pasted Graphic 4_1.jpg" alt="image description" />
+          </div>
+        </div>
+        <section className="text-gray-600 body-font bg-white -mt-[4em]">
+          <div className="container mx-auto flex px-5 py-20 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 ">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900 uppercase">About
+                <span className='text-[#990000]'>Admission</span>
+              </h1>
+              <h1 className="">ADMISSION INFORMATION</h1>
+              <p className="mb-8 leading-relaxed text-justify">The third week of November is always when the Common Aptitude Analysis for admission to Mayo College Girls' School is held at various locations in India. Additionally, it can be organised through the Indian Embassy there. The analysis analyses the applicant in English, Hindi, and mathematics to gauge their proficiency with those subjects as appropriate for their age. In August of the year before the desired admittance, parents are contacted.
+                Only students who have registered for admission and are eligible based on their previous class of study are permitted to take the aptitude analysis.
+                The lowest class is Class IV, and your child who has completed Class III and attends a school that teaches English, ideally, is eligible to take the Common Aptitude Analysis. Age restrictions are tightly upheld, and the parent is in charge of making the necessary arrangements for compliance.
+
+              </p>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img className="object-cover object-center rounded md:w-[80%]" alt="hero" src="image/Pasted Graphic 5_1.jpg" />
+            </div>
+          </div>
+        </section>
+
+
+      </div>
     </Layout>
   )
 }
