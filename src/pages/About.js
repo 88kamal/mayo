ddca6812a1ec85661c2a0956378232a66bc9c3da
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-12-05 09:51:08 +0530

    okk

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,7 +32,7 @@ function About() {
           </div>
         </section> */}
 
-<section className="bg-white dark:bg-gray-900 -mt-[4em]">
+<section className="bg-white  -mt-[4em]">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900 uppercase">About
